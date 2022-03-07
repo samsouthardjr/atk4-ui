47346094ca9d57bc5ac483fd68fdb0e1d4a7bd61
Michael Voříšek <mvorisek@mvorisek.cz>
2022-03-07 02:27:11 +0100

    simplify code

--- src/Table/Column/Flag.php
+++ src/Table/Column/Flag.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Atk4\Ui\Table\Column;
 
 use Atk4\Data\Model;
-use Atk4\Ui\Exception;
 use Atk4\Ui\Table;
 
 /**
@@ -13,40 +12,22 @@ use Atk4\Ui\Table;
  */
 class Flag extends Table\Column
 {
-    /**
-     * Name of model field which contains country ALPHA-2 (2 letter) codes.
-     *
-     * @var string
-     */
+    /** @var string Name of model field which contains country ALPHA-2 (2 letter) codes. */
     public $code_field;
 
-    /**
-     * Optional name of model field which contains country names.
-     *
-     * @var string
-     */
+    /** @var string|null Optional name of model field which contains country names. */
     public $name_field;
-
-    protected function init(): void
-    {
-        parent::init();
-
-        if (!$this->code_field) {
-            throw new Exception('Country code field must be defined');
-        }
-    }
 
     public function getHtmlTags(Model $row, $field)
     {
-        //if ($row->hasField($this->code_field)) {
-            $code = $row->get($this->code_field);
-            $name = $this->name_field ? $row->get($this->name_field) : null;
+        $countryCode = $row->get($this->code_field);
+        $countryName = $this->name_field ? $row->get($this->name_field) : null;
 
-            return [
-                $field->short_name => empty($code) ? '' : $this->getApp()->getTag('i', ['class' => strtolower($code) . ' flag', 'title' => $code . ($name ? ': ' . $name : '')]),
-            ];
-        //}
-
-        return [$field->short_name => $field->get($row)];
+        return [
+            $field->short_name => $countryCode === null ? '' : $this->getApp()->getTag('i', [
+                    'class' => strtolower($countryCode) . ' flag',
+                    'title' => $countryCode . ($countryName === null ? '' : ' - ' . $countryName)
+                ]),
+        ];
     }
 }
